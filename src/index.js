cd3193ff4f32653d2f71edfd7333a65917e44de9
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-22 13:40:46 +0300

    Add DOM and pure js examples

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import math from './math.js';
+let button = document.querySelector('#button');
+console.log({button});
+console.log(button.classList);
+button.addEventListener('click', () => {
+    if(button.classList.contains('is-primary')) {
+        //button.classList.toggle('is-success');
+        button.classList.replace('is-primary', 'is-success');
+    } else {
+        //button.classList.toggle('is-success');
+        button.classList.replace('is-success', 'is-primary');
+    }
+});
 
-let answer = math.add(2, 4);
-console.log(answer);
+let textInput = document.querySelector('#text');
+let reverseText = document.querySelector('#reverse-text');
+textInput.addEventListener('input', () => {
+    let value = textInput.value;
+    reverseText.innerText = value.split('').reverse().join('');
+});
