c9cdc099b5fc8cbcd341affe6e7bd9bc8be22958
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-22 14:13:27 +0300

    Add vue

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-let button = document.querySelector('#button');
-console.log({button});
-console.log(button.classList);
-button.addEventListener('click', () => {
-    if(button.classList.contains('is-primary')) {
-        //button.classList.toggle('is-success');
-        button.classList.replace('is-primary', 'is-success');
-    } else {
-        //button.classList.toggle('is-success');
-        button.classList.replace('is-success', 'is-primary');
-    }
-});
+import { createApp } from 'vue';
 
-let textInput = document.querySelector('#text');
-let reverseText = document.querySelector('#reverse-text');
-textInput.addEventListener('input', () => {
-    let value = textInput.value;
-    reverseText.innerText = value.split('').reverse().join('');
-});
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
